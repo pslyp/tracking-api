6e2db5a090733daa172c8ddca07ee7f959b13541
PSlyp <nonny_038@hotmail.com>
2020-06-12 12:29:16 +0700

    Initial commit

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ app = FastAPI()
 async def root():
     return "Welcome to track api."
 
-# if __name__ == '__main__':
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=5000, debug=True)
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8080, debug=True)
